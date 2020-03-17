cd4a4b1b7b7ccabdedba4e3c9513dacbae295fd9
Vicente Noriega <vicente.noriega.sc@gmail.com>
2020-03-17 16:17:33 -0700

    28. React, Router, first Functional component - branch16

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Header from './components/Layout/Header';
 import Dashboard from './components/Dashboard';
+import AddProject from './components/Project/AddProject';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
 	return (
-		<div className="App">
-			<Header />
-			<Dashboard />
-		</div>
+		<Router>
+			<div className="App">
+				<Header />
+				<Route exact path="/dashboard" component={Dashboard} />
+				<Route exact path="/addProject" component={AddProject} />
+			</div>
+		</Router>
 	);
 }
 
