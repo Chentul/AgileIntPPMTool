a809a3ea2bbcf37c8765ae6e730980762ea3527e
Vicente Noriega <vicente.noriega.sc@gmail.com>
2020-03-16 23:20:09 -0700

    25. Project and header components - branch13

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Header from './Layout/Header';
+import ProjectItem from './Project/ProjectItem';
 
 export default class Dashboard extends Component {
   render () {
     return(
       <div>
-        <h1>Welcome to Dashboard</h1>
+        <Header />
+        <ProjectItem />
       </div>
     );
   }
