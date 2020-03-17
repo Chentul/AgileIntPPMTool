92e98703e7a35e2bec8c568e2f0c2d74f4dd537e
Vicente Noriega <vicente.noriega.sc@gmail.com>
2020-03-17 11:06:56 -0700

    27. Style our Dashboard, Navbar, ProjectItem - branch15.

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import Header from './components/Layout/Header';
 import Dashboard from './components/Dashboard';
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Dashboard />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Dashboard />
+		</div>
+	);
 }
 
 export default App;
